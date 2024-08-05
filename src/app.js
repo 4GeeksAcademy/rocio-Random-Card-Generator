a8f712cb7897ac2ba2531c9e5ab665ad7cccaaa9
rociomr08 <161978284+rociomr08@users.noreply.github.com>
2024-08-05 22:32:08 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,57 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+// Corazones, Picas, Tréboles y Diamantes.
+// corazones y diamantes =>color rojo
+//trebol y espadas=> color negro
+
+let card = ["♥", "♠", "♣", "♦"];
+
+let valueCard = [
+  "A",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K"
+];
+
+function randomNum(arr) {
+  let random = Math.floor(Math.random() * arr.length);
+  return arr[random];
+}
+
+//Probando en consola
+console.log(randomNum(card));
+console.log(randomNum(valueCard));
+
+function generatorCard() {
+  let palo = randomNum(card);
+  let valor = randomNum(valueCard);
+
+  let colorCard;
+  if (palo === "♥" || palo === "♦") {
+    colorCard = "red";
+  } else {
+    colorCard = "black";
+  }
+
+  document.getElementById("card").innerHTML = `
+    <div class="symbol top-left ${colorCard}">${palo}</div>
+    <div class="value">${valor}</div>
+    <div class="symbol bottom-right ${colorCard}">${palo}</div>
+  `;
+}
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  generatorCard();
+  document.getElementById("newCard").addEventListener("click", generatorCard);
+  setInterval(generatorCard, 10000);
 };
